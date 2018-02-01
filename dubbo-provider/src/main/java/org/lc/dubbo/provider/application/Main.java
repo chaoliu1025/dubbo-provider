6cb1f391548c4cd00d3aab156dda25e7017305b8
chaoliu1025 <chaoliu1025@163.com>
2018-02-01 17:56:19 +0800

    dubbo provider

---
+++ dubbo-provider/src/main/java/org/lc/dubbo/provider/application/Main.java
@@ -0,0 +1,9 @@
+package org.lc.dubbo.provider.application;
+
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		args = new String[]{"spring","log4j"};
+		com.alibaba.dubbo.container.Main.main(args);
+    }
+}
